eab386cbf87c604f4c95e4b272352d068f85375d
tmsheak <tmsheak@gmail.com>
2023-02-21 10:33:29 -0500

    practicing git commands

---
+++ src/test/java/facebookGitPage2/GitPractice2.java
@@ -0,0 +1,11 @@
+package facebookGitPage2;
+
+public class GitPractice2 {
+
+	public static void main(String[] args) {
+		System.out.println("Create new Branch and Push.");
+		System.err.println("Pull Request, Review and Merge");
+
+	}
+
+}
